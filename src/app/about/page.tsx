3477518fdce41c863ff63a18cbd96d226739688e
develnom <develnom@gmail.com>
2023-07-10 23:51:50 +0900

    about, contact 페이지 작업

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,3 +1,24 @@
+import Hero from "@/components/Hero";
+
+const TITLE_CLASS = "text-2xl font-bold text-gray-800 my-2";
 export default function AboutPage() {
-  return <p>About</p>;
+  return (
+    <>
+      <Hero />
+      <section className="bg-gray-100 shadow-lg p-8 m-8 text-center">
+        <h2 className={TITLE_CLASS}>Who Am I?</h2>
+        <p>
+          개발을 사랑하는 풀스택 개발자 <br />
+          사람과 디자인을 담는 웹앱을 만들고 있음
+        </p>
+        <h2 className={TITLE_CLASS}>Career</h2>
+        <p>비스톤스 (-Now)</p>
+        <p>지원앤컴 (-2019)</p>
+        <h2 className={TITLE_CLASS}>Skills</h2>
+        <p>React, Vue, Node</p>
+        <p>Git, Clean Code</p>
+        <p>VS Code</p>
+      </section>
+    </>
+  );
 }
